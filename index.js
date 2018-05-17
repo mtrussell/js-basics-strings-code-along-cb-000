5d196a6f22c69edd75b7ddb01d270551e36fb60a
Matt Trussell <smtrussell@gmail.com>
2018-05-17 21:47:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let currentUser = "Matt";
 
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
